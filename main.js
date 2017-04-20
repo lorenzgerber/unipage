ff749367cb8fd406ab77da65129c332eed859c30
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-04-20 23:12:23 +0200

    tool setup

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
-  'use strict';
-  paper.install(window);
-  paper.setup(document.getElementById('mainCanvas'));
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
 
-  var c = Shape.Circle(200, 200, 50);
-  c.fillColor = 'green';
+    var tool = new Tool();
 
-  paper.view.draw();
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center'; text.fillColor = 'white'; text.fontSize = 20;
+    text.content = 'hello world';
+
+    tool.onMouseDown = function(event) {
+        var c = Shape.Circle(event.point.x, event.point.y, 20);
+        c.fillColor = 'green';
+    };
+
+    paper.view.draw();
 });
